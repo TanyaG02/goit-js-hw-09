611cdf019d9a7677d34fb38f2b5022b7f1c717d6
TanyaG02 <tanyagryanko@gmail.com>
2022-10-07 23:34:32 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    getRandomHexColor();
+  }, 1000);
+  if ((buttonStart.disabled = true)) {
+    buttonStop.disabled = false;
+  }
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  if ((buttonStop.disabled = true)) {
+    buttonStart.disabled = false;
+  }
+});
