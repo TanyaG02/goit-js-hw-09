6d528f673f6443b5320c6145808387b4e854a2a8
TanyaG02 <tanyagryanko@gmail.com>
2022-10-10 14:14:56 +0300

    new commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,43 +1,36 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const formRef = document.querySelector('.form');
-formRef.addEventListener('submit', onSubmitForm);
-
-function onSubmitForm(e) {
-  e.preventDefault();
-
-  let delay = Number(e.currentTarget.delay.value);
-  const step = Number(e.currentTarget.step.value);
-  const amount = Number(e.currentTarget.amount.value);
-
-  for (let position = 1; position <= amount; position += 1) {
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-            useIcon: false,
-          });
-        }, delay);
-      })
-      .catch(({ position, delay }) => {
-        setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-            useIcon: false,
-          });
-        }, delay);
-      });
-    delay += step;
-  }
-}
+const form = document.querySelector('.form');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const objectPromise = { position, delay };
-
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(objectPromise);
-    }
-    reject(objectPromise);
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
   });
 }
+
+const onSubmitClick = event => {
+  event.preventDefault();
+  const stepValue = Number(document.querySelector('[name="step"]').value);
+  const delayValue = Number(document.querySelector('[name="delay"]').value);
+  const amount = Number(document.querySelector('[name="amount"]').value);
+  let delayOver = delayValue;
+
+  for (let i = 0; i < amount; i += 1) {
+    const position = i + 1;
+    createPromise(position, delayOver + stepValue * i)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+};
+
+form.addEventListener('submit', onSubmitClick);
